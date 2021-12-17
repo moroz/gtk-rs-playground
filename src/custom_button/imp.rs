f7e8b928e683521b02b602bd579d2aa72749378c
Karol Moroz <k.j.moroz@gmail.com>
2021-12-17 16:49:01 +0100

    Increment number on button click

--- src/custom_button/imp.rs
+++ src/custom_button/imp.rs
@@ -1,9 +1,13 @@
 use gtk::glib;
+use gtk::prelude::*;
 use gtk::subclass::prelude::*;
+use std::cell::Cell;
 
 // Object holding the state
 #[derive(Default)]
-pub struct CustomButton;
+pub struct CustomButton {
+    number: Cell<i32>,
+}
 
 // The central trait for subclassing a GObject
 #[glib::object_subclass]
@@ -14,10 +18,20 @@ impl ObjectSubclass for CustomButton {
 }
 
 // Trait shared by all GObjects
-impl ObjectImpl for CustomButton {}
+impl ObjectImpl for CustomButton {
+    fn constructed(&self, obj: &Self::Type) {
+        self.parent_constructed(obj);
+        obj.set_label(&self.number.get().to_string());
+    }
+}
 
 // Trait shared by all widgets
 impl WidgetImpl for CustomButton {}
 
 // Trait shared by all buttons
-impl ButtonImpl for CustomButton {}
+impl ButtonImpl for CustomButton {
+    fn clicked(&self, button: &Self::Type) {
+        self.number.set(self.number.get() + 1);
+        button.set_label(&self.number.get().to_string())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use custom_button::CustomButton;
-use gtk::glib;
-use gtk::glib::*;
 use gtk::prelude::*;
 use gtk::Application;
 use gtk::ApplicationWindow;
-use gtk::Orientation;
-use std::cell::Cell;
-use std::rc::Rc;
 
 pub mod custom_button;
 
@@ -20,41 +15,20 @@ fn main() {
     app.run();
 }
 
-fn build_button(text: &str) -> CustomButton {
-    let button = CustomButton::with_label(text);
+fn build_ui(app: &Application) {
+    let button = CustomButton::new();
     button.set_margin_top(12);
     button.set_margin_bottom(12);
     button.set_margin_end(12);
     button.set_margin_start(12);
-    button
-}
-
-fn build_ui(app: &Application) {
-    let button_decrease = build_button("Decrease");
-    let button_increase = build_button("Increase");
-
-    let number = Rc::new(Cell::new(0));
-
-    button_increase.connect_clicked(clone!(@weak number, @weak button_decrease => move |_| {
-        number.set(number.get() + 1);
-        button_decrease.set_label(&number.get().to_string());
-    }));
-
-    button_decrease.connect_clicked(clone!(@weak button_increase =>
-    move |_| {
-        number.set(number.get() - 1);
-        button_increase.set_label(&number.get().to_string());
-    }));
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
+        .child(&button)
+        .default_width(120)
+        .default_height(120)
         .build();
-
-    let gtk_box = gtk::Box::new(Orientation::Vertical, 0);
-    window.set_child(Some(&gtk_box));
-    gtk_box.append(&button_increase);
-    gtk_box.append(&button_decrease);
 
     window.present();
 }
