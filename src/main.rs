0423eb405141fff7d25fa46334371d34fc4a851d
Karol Moroz <k.j.moroz@gmail.com>
2021-12-17 15:34:01 +0100

    Add counters in main application window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use gtk::glib;
+use gtk::glib::*;
 use gtk::prelude::*;
 use gtk::Application;
 use gtk::ApplicationWindow;
 use gtk::Button;
+use gtk::Orientation;
+use std::cell::Cell;
+use std::rc::Rc;
 
 fn main() {
     let app = Application::builder()
@@ -13,24 +18,42 @@ fn main() {
     app.run();
 }
 
-fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Press me!")
+fn build_button(text: &str) -> Button {
+    Button::builder()
+        .label(text)
         .margin_top(12)
         .margin_bottom(12)
         .margin_end(12)
         .margin_start(12)
-        .build();
+        .build()
+}
 
-    button.connect_clicked(move |button| {
-        button.set_label("Hello World!");
-    });
+fn build_ui(app: &Application) {
+    let button_decrease = build_button("Decrease");
+    let button_increase = build_button("Increase");
+
+    let number = Rc::new(Cell::new(0));
+
+    button_increase.connect_clicked(clone!(@weak number, @weak button_decrease => move |_| {
+        number.set(number.get() + 1);
+        button_decrease.set_label(&number.get().to_string());
+    }));
+
+    button_decrease.connect_clicked(clone!(@weak button_increase =>
+    move |_| {
+        number.set(number.get() - 1);
+        button_increase.set_label(&number.get().to_string());
+    }));
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
         .build();
+
+    let gtk_box = gtk::Box::new(Orientation::Vertical, 0);
+    window.set_child(Some(&gtk_box));
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     window.present();
 }
