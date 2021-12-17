1993b9a4bfb5a8d5575ac3fffc3885fe324839a3
Karol Moroz <k.j.moroz@gmail.com>
2021-12-17 15:11:02 +0100

    First GTK window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use gtk::prelude::*;
+use gtk::Application;
+use gtk::ApplicationWindow;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.gtk-rs.example")
+        .build();
+
+    app.connect_activate(build_ui);
+
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
+
+    window.present();
 }
